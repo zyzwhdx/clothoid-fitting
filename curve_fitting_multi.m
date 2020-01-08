78e041ab8cd7e27e175715e40dbce3d8bf8a2534
zyzwhdx <zyzwhdx@163.com>
2020-01-08 20:58:13 +0800

    多段可视化

--- curve_fitting_multi.m
+++ curve_fitting_multi.m
@@ -47,23 +47,37 @@ for i = 2: total_point_number - 1
 end
 
 %% 优化
-l_bound = zeros(3*segment_number,1); h_bound = zeros(3*segment_number,1);
-    for i = 1:segment_number
-        l_bound(3*i-2) = 0.0;
-        l_bound(3*i-1) = -0.05;
-        l_bound(3*i-0) = -0.0025;
-        h_bound(3*i-2) = 2*pi;
-        h_bound(3*i-1) = 0.05;
-        h_bound(3*i-0) = 0.0025;
-    end
-greek = optimvar('greek', 3*segment_number, 'LowerBound', l_bound, 'UpperBound', h_bound); % 要迭代的变量，每一段三个
 
+% 迭代变量
+l_bound = zeros(3*segment_number,1); h_bound = zeros(3*segment_number,1);
+for i = 1:segment_number
+    l_bound(3*i-2) = 0.0;
+    l_bound(3*i-1) = -0.05;
+    l_bound(3*i-0) = -0.0025;
+    h_bound(3*i-2) = 2*pi;
+    h_bound(3*i-1) = 0.05;
+    h_bound(3*i-0) = 0.0025;
+end
+greek = optimvar('greek', 3*segment_number, 'LowerBound', l_bound, 'UpperBound', h_bound); % 要迭代的变量，每一段三个
+% 变量初值
+x0.greek = zeros(1, 3*segment_number);
+for i = 1:segment_number
+    x0.greek(3*i-2) = 1.5;
+    x0.greek(3*i-1) = 0.0;
+    x0.greek(3*i-0) = 0.0;
+end
+
+% 积分函数
 diffexpr = 0.0;
 x_integ = @(t, mu, ka, ps) cos(mu + ka.*t + 0.5.*ps.*t.*t);
 y_integ = @(t, mu, ka, ps) sin(mu + ka.*t + 0.5.*ps.*t.*t);
-% 迭代初值
 
-% 遍历每一段
+% 存储结果
+coor_begin = zeros(segment_number, 2);
+greek_result = zeros(segment_number, 2);
+seg_length_result = zeros(segment_number, 1);
+
+% 遍历每一段，segment_iter
 for segment_iter = 1 : 4
    % 取出坐标
    coor_segment = coor(segment_label(segment_iter,1):segment_label(segment_iter,2),:);
@@ -71,6 +85,7 @@ for segment_iter = 1 : 4
    distance_accu_seg = zeros(seg_point_number, 1);
    x00 = coor_segment(1,1); y00 = coor_segment(1,2);
 
+    % 计算u
     for i = 2 : seg_point_number
         x1 = coor_segment(i,1); y1 = coor_segment(i,2);
         x2 = coor_segment(i-1,1); y2 = coor_segment(i-1,2);
@@ -85,6 +100,7 @@ for segment_iter = 1 : 4
     distance_ratio(end) = 1;
     u = distance_ratio;
     
+    % 目标函数计算
     diffun{segment_iter*2-1} = @(greek, uu) x00 + integral(@(t)x_integ(t, ...
     greek(segment_iter*3-2), greek(segment_iter*3-1), greek(segment_iter*3)), 0, uu*curve_length_seg);
     diffun{segment_iter*2} = @(greek, uu) y00 + integral(@(t)y_integ(t, ...
@@ -94,36 +110,39 @@ for segment_iter = 1 : 4
         diffexpr = diffexpr + (fcn2optimexpr(diffun{segment_iter*2-1}, greek, u(i)) - coor_segment(i,1)).^2;
         diffexpr = diffexpr + (fcn2optimexpr(diffun{segment_iter*2}, greek, u(i)) - coor_segment(i,2)).^2;
     end
+    
+    coor_begin(segment_iter, 1) = x00; coor_begin(segment_iter, 2) = y00;
+    seg_length_result(segment_iter, 1) = curve_length_seg;
 end
 
-    x0.greek = zeros(1, 3*segment_number);
-    for i = 1:segment_number
-        x0.greek(3*i-2) = 1.5;
-        x0.greek(3*i-1) = 0.0;
-        x0.greek(3*i-0) = 0.0;
-    end
-    ssqprob = optimproblem('Objective', diffexpr);
-    options = optimoptions('lsqlin');
-    options.Display = 'iter';
-    showproblem(ssqprob)
-    [sol, fval, exitflag, output] = solve(ssqprob, x0, 'Options', options); 
-    sol.greek
-    
-    %% curve generator
-    s = linspace(0, curve_length_seg, 100);
+% solver生成
+ssqprob = optimproblem('Objective', diffexpr);
+options = optimoptions('lsqlin');
+options.Display = 'iter';
+options.MaxIterations = 200;
+showproblem(ssqprob)
+[sol, fval, exitflag, output] = solve(ssqprob, x0, 'Options', options); 
+sol.greek
+
+%% 结果对比
+for segment_iter = 1 : 4
+    s = linspace(0, seg_length_result(segment_iter), 500);
     coor2 = zeros(100, 2);
-    coor2(1,1) = x00; coor2(1,2) = y00;
-    mu0 = sol.greek(10); kappa0 = sol.greek(11); psi = sol.greek(12);
-    for i = 1 : 100
+    coor2(1,1) = coor_begin(segment_iter, 1); coor2(1,2) = coor_begin(segment_iter, 2);
+    mu0 = sol.greek(3*segment_iter-2); kappa0 = sol.greek(3*segment_iter-1); psi = sol.greek(3*segment_iter);
+    for i = 1 : 500
         % 积分
         fun1 = @(t, mu0, kappa0, psi) cos(mu0 + t.*kappa0 + 0.5.*t.*t.*psi);
         fun2 = @(t, mu0, kappa0, psi) sin(mu0 + t.*kappa0 + 0.5.*t.*t.*psi);
         Q1 = integral(@(t)fun1(t,mu0,kappa0,psi),s(1),s(i));
         Q2 = integral(@(t)fun2(t,mu0,kappa0,psi),s(1),s(i));
-        coor2(i,1) = Q1 + x00;
-        coor2(i,2) = Q2 + y00;
+        coor2(i,1) = Q1 + coor2(1,1);
+        coor2(i,2) = Q2 + coor2(1,2);
     end
     plot(coor2(:,1),coor2(:,2),'b')
     hold on
-    scatter(coor_segment(:,1),coor_segment(:,2), 'r')
+    scatter(coor(segment_label(segment_iter,1):segment_label(segment_iter,2),1),coor(segment_label(segment_iter,1):segment_label(segment_iter,2),2), 'r')
+end
+
+
 
